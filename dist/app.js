69937930016eae7f8b6c018eb73a428ed946c344
Lazaro <lazaro.fonseca@ebix.com>
2019-09-25 21:06:11 -0300

    setting up project for development

---
+++ dist/app.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express = require("express");
+class App {
+    constructor() {
+        this.express = express();
+        this.middleware();
+    }
+    middleware() {
+        this.express.use("/index", (req, res, next) => {
+            res.send({
+                hello: "index!"
+            });
+        });
+    }
+}
+exports.default = new App().express;
